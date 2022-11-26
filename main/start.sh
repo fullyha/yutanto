0b2ac9df2ea42b48bd27a7354a8a3048d81a33ee
fullyha <116690845+fullyha@users.noreply.github.com>
2022-11-26 15:33:55 +0000

    Update start.sh

--- main/start.sh
+++ main/start.sh
@@ -1,5 +1,5 @@
 export PORT=${PORT-8080}
-export UUID=${UUID-6e1b41f8b074449b9cd6ff53d7159bf8}
+export UUID=${UUID-7a90ddb96936425094a23a169b186f6d}
 export PATH_vless=${PATH_vless-/vless/$UUID}
 export PATH_trojan=${PATH_trojan-/trojan/$UUID}
 export PATH_vmess=${PATH_vmess-/vmess/$UUID}
